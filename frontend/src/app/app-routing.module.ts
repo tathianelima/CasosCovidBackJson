c39b5be548e6bfaee422bd810b94ac0de87a060e
Tathiane Lima <tathiane.lima@hotmail.com>
2020-11-01 16:36:14 -0300

    Criação das views Home, Dados e Gráfico e configuração das rotas de navegação

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './views/home/home.component';
+import { DadosComponent } from './views/dados/dados.component';
+import { GraficoComponent } from './views/grafico/grafico.component';
+
+const routes: Routes = [
+  {
+  path: "",
+  component: HomeComponent
+  },
+  {
+  path: "dados",
+  component: DadosComponent
+  },
+  {
+  path: "grafico",
+  component: GraficoComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
